dd043ac7640be002b72801f97b33564e1ed10a45
Nasir Saeed <saeednasir483@gmail.com>
2025-03-03 15:51:15 +0500

    set card title

--- components/ProductCardUI/index.js
+++ components/ProductCardUI/index.js
@@ -6,15 +6,36 @@ import classes from "./ProductCardUI.module.css";
 export default function ProductCardUI({ data }) {
 
     return (
-        <Card style={{ width: '19rem' }}>
-            <div className={classes.product_card_image}>
-                <Image src={data?.thumbnail} fill alt={data?.title} />
-            </div>
-            <Card.Body>
-                <Link href={`/products/${data?.id}`}><h3>{data?.title}</h3></Link>
-                <p> {data?.description?.split(" ").slice(0, 15).join(" ")}</p>
-            </Card.Body>
-        </Card>
+        <>
+            <style>
+                {
+                    `
+                .card-body a{
+                    text-decoration: none;
+                    color: #222222;
+                }
+
+                .card-body h3 {
+                    font-family: var(--font-poppins);
+                    font-weight: 600;
+                    font-size: 24px;
+                    color: #222222;
+                }
+                `
+                }
+            </style>
+
+            <Card style={{ width: '19rem' }}>
+                <div className={classes.product_card_image}>
+                    <Image src={data?.thumbnail} fill alt={data?.title} />
+                </div>
+                <Card.Body>
+                    <Link href={`/ products / ${data?.id}`}><h3>{data?.title}</h3></Link>
+                    <p> {data?.description?.split(" ").slice(0, 15).join(" ")}</p>
+                </Card.Body >
+            </Card >
+        </>
+
     );
 }
 
